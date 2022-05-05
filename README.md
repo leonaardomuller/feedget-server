2fdc7cbca4a45a5a28627926b31cc6343abd8c24
Leonardo Muller <leonardo.muller@fockink.ind.br>
2022-05-05 17:48:10 -0300

    Stage 3 - 01:05:00

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-npm run dev
+# SOLID
 
+1 Single responsibility
+    Cada classe tem uma responsabilidade única
+
+2 Open/Closed
+    As classes da nossa aplicação devem ser abertas para extensão mas fechadas para modificação.
+
+3 Liskov Substitution
+    Nós devemos poder substituir uma classe pai por uma herança dela e tudo continuar funcionando.
+
+4 Interface Segregation
+...
+5 Dependency Inversion
+    Módulos de alto nível não devem depender de módulos de baixo nível; ambos devem depender de abstrações .
+
+
+npx prisma init  
 npx prisma migrate dev
-npx prisma init   
 npx prisma studio
+
+npm run dev
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,15 @@ const app = express();
 
 app.use(express.json());
 
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "85b321fa6a8ade",
+    pass: "6d13d7bb6964c7",
+  },
+});
+
 app.post("/feedbacks", async (request, response) => {
   const { type, comment, screenshot } = request.body;
 
@@ -15,6 +24,18 @@ app.post("/feedbacks", async (request, response) => {
       comment,
       screenshot,
     },
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Leonardo Muller <leonaardo.muller@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111"`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment} </p>`,
+      `</div>`,
+    ].join("\n"),
   });
 
   return response.status(201).json({ data: feedback });
