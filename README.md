139d2bc986993a8dafd8ec2d372d1396114ded67
leonaardomuller <leonaardo.muller@gmail.com>
2022-05-04 22:37:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+npm run dev
+
+npx prisma migrate dev
+npx prisma init   
+npx prisma studio
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import nodemailer from "nodemailer";
+import { prisma } from "./prisma";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/feedbacks", async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return response.status(201).json({ data: feedback });
+});
+
+app.listen(3333, () => console.log("HTTP server running"));
